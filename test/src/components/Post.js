9ee7c70cd287d568013d81665ff0140ded7fd8e2
Jerome <jerome0caruso@gmail.com>
2021-10-26 19:19:40 -0500

    fixed last_name

--- test/src/components/Post.js
+++ test/src/components/Post.js
@@ -6,7 +6,7 @@ const postComponent = (props) => {
         <tr>
             <td>{props.post.id}</td>
             <td>{props.post.date_created}</td>
-            <td>{props.post.user.first_name} {props.post.user.first_name}</td>
+            <td>{props.post.user.first_name} {props.post.user.last_name}</td>
             <td>{props.post.user.id}</td>
             <td>{props.post.title}</td>
             <td>{props.post.body}</td>
